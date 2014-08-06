f06bed55903b5a4381c7bff0c146b347bbc1712a
Jacob Poul Richardt <spam@poulrichardt.dk>
2014-08-06 17:05:06 +0200

    Added menu

--- src/app/Cockpit.Web/App/ViewModels/Shell.js
+++ src/app/Cockpit.Web/App/ViewModels/Shell.js
@@ -1,7 +1,8 @@
-﻿define(["require", "exports", "Navigation", "less!Styles/Shell"], function(require, exports, Navigation) {
+﻿define(["require", "exports", "Navigation", "AuthenticationManager", "less!Styles/Shell"], function(require, exports, Navigation, AuthenticationManager) {
     var Shell = (function () {
         function Shell() {
             this.Page = Navigation.CurrentPage;
+            this.IsAuthenticated = AuthenticationManager.IsAuthenticated;
         }
         return Shell;
     })();
--- src/app/Cockpit.Web/App/ViewModels/Shell.ts
+++ src/app/Cockpit.Web/App/ViewModels/Shell.ts
@@ -1,14 +1,17 @@
 ﻿/// <amd-dependency path="less!Styles/Shell" />
 import Navigation = require("Navigation");
+import AuthenticationManager = require("AuthenticationManager");
 import Page = require("ViewModels/Page");
 
 class Shell
 {
 	public Page: KnockoutObservable<Page>;
+	public IsAuthenticated:KnockoutObservable<boolean>;
 
 	constructor()
 	{
 		this.Page = Navigation.CurrentPage;
+		this.IsAuthenticated = AuthenticationManager.IsAuthenticated;
 	}
 }
 
