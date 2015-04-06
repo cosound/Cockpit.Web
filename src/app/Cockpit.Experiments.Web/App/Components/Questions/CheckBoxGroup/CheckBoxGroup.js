cf04d905d1364c907019f3a6c78e37f990059016
Jacob Poul Richardt <spam@poulrichardt.dk>
2015-04-06 07:22:03 +0200

    Max selections is now enforced for checkbox group.

--- src/app/Cockpit.Experiments.Web/App/Components/Questions/CheckBoxGroup/CheckBoxGroup.js
+++ src/app/Cockpit.Experiments.Web/App/Components/Questions/CheckBoxGroup/CheckBoxGroup.js
@@ -14,11 +14,22 @@ define(["require", "exports", "knockout", "Components/Questions/QuestionBase"], 
             this.Id = this.Model.Id;
             this.Label = this.GetInstrument("HeaderLabel");
             this.Url = this.GetInstrument("Stimulus");
-            this.Items = this.GetInstrument("Items").Item;
+            this._minNoOfSelections = this.GetInstrument("MinNoOfSelections");
+            this._maxNoOfSelections = this.GetInstrument("MaxNoOfSelections");
+            this.CanSelectMore = knockout.computed(function () { return _this.Answer().length < _this._maxNoOfSelections; });
+            this.Items = this.GetInstrument("Items").Item.map(function (v) { return _this.CreateCheckBoxInfo(v); });
             if (this.HasAnswer())
                 this.Answer.push.apply(this.Answer, this.GetAsnwer()["Selections"]);
-            this.Answer.subscribe(function (v) { return _this.SetAnswer({ Selections: _this.Answer() }); });
         }
+        CheckBoxGroup.prototype.CreateCheckBoxInfo = function (data) {
+            var _this = this;
+            var info = {
+                Id: data.Id,
+                Label: data.Label,
+                IsEnabled: knockout.computed(function () { return _this.Answer.indexOf(data.Id) != -1 || _this.CanSelectMore(); })
+            };
+            return info;
+        };
         return CheckBoxGroup;
     })(QuestionBase);
     return CheckBoxGroup;
--- src/app/Cockpit.Experiments.Web/App/Components/Questions/CheckBoxGroup/CheckBoxGroup.ts
+++ src/app/Cockpit.Experiments.Web/App/Components/Questions/CheckBoxGroup/CheckBoxGroup.ts
@@ -2,13 +2,19 @@
 import QuestionBase = require("Components/Questions/QuestionBase");
 import QuestionModel = require("Models/Question");
 
+type CheckBoxInfo = { Id:string; Label: string; IsEnabled: KnockoutComputed<boolean>; };
+
 class CheckBoxGroup extends QuestionBase
 {
 	public Id: string;
 	public Label: string;
 	public Url: string;
-	public Items: string[];
+	public Items: CheckBoxInfo[];
 	public Answer: KnockoutObservableArray<string> = knockout.observableArray<string>();
+	public CanSelectMore:KnockoutComputed<boolean>;
+
+	private _minNoOfSelections:number;
+	private _maxNoOfSelections:number;
 
 	constructor(question: QuestionModel)
 	{
@@ -17,11 +23,25 @@ class CheckBoxGroup extends QuestionBase
 		this.Id = this.Model.Id;
 		this.Label = this.GetInstrument("HeaderLabel");
 		this.Url = this.GetInstrument("Stimulus");
+		this._minNoOfSelections = this.GetInstrument("MinNoOfSelections");
+		this._maxNoOfSelections = this.GetInstrument("MaxNoOfSelections");
 
-		this.Items = this.GetInstrument("Items").Item;
+		this.CanSelectMore = knockout.computed(() => this.Answer().length < this._maxNoOfSelections);
+
+		this.Items = (<any[]>this.GetInstrument("Items").Item).map(v => this.CreateCheckBoxInfo(v));	
 
 		if (this.HasAnswer()) this.Answer.push.apply(this.Answer, this.GetAsnwer()["Selections"]);
-		this.Answer.subscribe(v => this.SetAnswer({ Selections: this.Answer() }));
+	}
+
+	private CreateCheckBoxInfo(data:{Label:string; Id:string}):CheckBoxInfo
+	{
+		var info = {
+			Id: data.Id,
+			Label: data.Label,
+			IsEnabled: knockout.computed(() => this.Answer.indexOf(data.Id) != -1 || this.CanSelectMore())
+		};
+
+		return info;
 	}
 }
 
