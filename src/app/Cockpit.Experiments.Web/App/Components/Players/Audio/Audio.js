b77cc24f2a63f297ea36916277bac04c0aec524d
Jacob Poul Richardt <spam@poulrichardt.dk>
2015-05-08 12:22:32 +0200

    Updated player to only be play/pause. Only one player can play at a time.

--- src/app/Cockpit.Experiments.Web/App/Components/Players/Audio/Audio.js
+++ src/app/Cockpit.Experiments.Web/App/Components/Players/Audio/Audio.js
@@ -4,20 +4,36 @@ define(["require", "exports", "knockout", "jquery"], function (require, exports,
             var _this = this;
             this.PlayerElement = knockout.observable();
             this._info = info;
+            this.IsPlaying = this._info.IsPlaying;
             this.Sources = this._info.Sources;
             var sub = this.PlayerElement.subscribe(function (e) {
                 sub.dispose();
                 _this.InitializePlayer(e);
             });
         }
+        Audio.prototype.TogglePlay = function () {
+            if (this.IsPlaying()) {
+                Audio._activePlayer = null;
+                this.PlayerElement().pause();
+            }
+            else {
+                if (Audio._activePlayer !== null && Audio._activePlayer !== this)
+                    Audio._activePlayer.TogglePlay();
+                Audio._activePlayer = this;
+                this.PlayerElement().play();
+            }
+        };
         Audio.prototype.InitializePlayer = function (player) {
             var _this = this;
-            jquery(player).on("playing", function () {
+            var $player = jquery(player);
+            $player.on("playing", function () {
                 _this._info.IsPlaying(true);
             }).on("pause", function () {
-                _this._info.IsPlaying(true);
+                _this._info.IsPlaying(false);
             });
+            this.Sources.forEach(function (s) { return $player.append("<Source type=\"" + s.Type + "\" src=\"" + s.Source + "\"/>"); });
         };
+        Audio._activePlayer = null;
         return Audio;
     })();
     return Audio;
--- src/app/Cockpit.Experiments.Web/App/Components/Players/Audio/Audio.ts
+++ src/app/Cockpit.Experiments.Web/App/Components/Players/Audio/Audio.ts
@@ -8,12 +8,16 @@ class Audio
 {
 	public PlayerElement: KnockoutObservable<HTMLAudioElement> = knockout.observable<HTMLAudioElement>();
 	public Sources: Source[];
+	public IsPlaying: KnockoutObservable<boolean>;
 
 	private _info: AudioInfo;
+	private static _activePlayer:Audio = null;
+
 
 	constructor(info:AudioInfo)
 	{
 		this._info = info;
+		this.IsPlaying = this._info.IsPlaying;
 
 		this.Sources = this._info.Sources;
 
@@ -21,18 +25,40 @@ class Audio
 		{
 			sub.dispose();
 			this.InitializePlayer(e);
-		})
+		});
+	}
+
+	public TogglePlay():void
+	{
+		if (this.IsPlaying())
+		{
+			Audio._activePlayer = null;
+			this.PlayerElement().pause();
+		}
+		else
+		{
+			if (Audio._activePlayer !== null && Audio._activePlayer !== this)
+				Audio._activePlayer.TogglePlay();
+
+			Audio._activePlayer = this;
+			this.PlayerElement().play();
+		}
+			
 	}
 
 	private InitializePlayer(player:HTMLAudioElement):void
 	{
-		jquery(player).on("playing", () =>
+		var $player = jquery(player);
+
+		$player.on("playing", () =>
 		{
 			this._info.IsPlaying(true);
 		}).on("pause", () =>
 		{
-			this._info.IsPlaying(true);
+			this._info.IsPlaying(false);
 		});
+
+		this.Sources.forEach(s => $player.append(`<Source type="${s.Type}" src="${s.Source}"/>`));
 	}
 }
 
