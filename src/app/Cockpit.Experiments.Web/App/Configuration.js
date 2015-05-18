41a51831fee3e0aec7ed92c34be66574d115207f
Jacob Poul Richardt <spam@poulrichardt.dk>
2015-05-18 13:06:07 +0200

    Rebuild config.

--- src/app/Cockpit.Experiments.Web/App/Configuration.js
+++ src/app/Cockpit.Experiments.Web/App/Configuration.js
@@ -1,6 +1,7 @@
 define(["require", "exports"], function (require, exports) {
-    exports.PortalPath = "https://dev-api.cosound.dk";
+    exports.PortalPath = "https://prod-api.cosound.dk";
     exports.ExperimentTitle = "Cockpit";
     exports.IsPoweredByCockpitVisible = true;
+    exports.CloseSlides = true;
     exports.SlideName = "slide";
 });
